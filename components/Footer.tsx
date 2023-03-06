2c57e59e386fadf52f7b677ed0bafc3023e71196
Vicky Adi Firmansyah <vickyadi243@gmail.com>
2023-03-06 22:21:04 +0700

    fix: link footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,15 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="p-4 bg-white md:px-6 md:py-8 border-t-[1px] mt-28 font-main">
       <div className="sm:flex sm:items-center sm:justify-between">
-        <a
-          href="https://flowbite.com/"
-          className="flex items-center mb-4 sm:mb-0"
-        >
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+        <Link href="/" className="flex items-center mb-4 sm:mb-0">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-gray-900">
+            Meals
           </span>
-        </a>
-        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400 space-x-5">
+        </Link>
+        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-900 sm:mb-0 space-x-5">
           <li>About</li>
           <li>Privacy Policy</li>
           <li>Licensing</li>
